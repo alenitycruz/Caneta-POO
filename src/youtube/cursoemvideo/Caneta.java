49738389a2a0d2e7350b4e2360e58bf732ffd58a
alenitycruz <alessandradsmcruz@recife.pe.senac.br>
2022-03-22 08:15:32 -0300

    inserido metodos getter, setter e constructor

--- src/youtube/cursoemvideo/Caneta.java
+++ src/youtube/cursoemvideo/Caneta.java
@@ -10,11 +10,57 @@ public class Caneta {
 
 
     public void status(){
-        System.out.println("Modelo " + this.modelo);
-        System.out.println("Cor " + this.cor);
-        System.out.println("Ponta " + this.ponta);
-        System.out.println("Carga " + this.carga);
-        System.out.println("Está tampada - " + this.tampada);
+        System.out.println("Modelo " + this.getModelo());
+        System.out.println("Cor " + this.getCor());
+        System.out.println("Ponta " + this.getPonta());
+        System.out.println("Carga " + this.getCarga());
+        System.out.println("Está tampada - " + this.isTampada());
+    }
+
+    public Caneta() {
+        tampar();
+        this.setModelo("Faber Castel");
+        this.setPonta(0.7f);
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public float getPonta() {
+        return ponta;
+    }
+
+    public void setPonta(float ponta) {
+        this.ponta = ponta;
+    }
+
+    public int getCarga() {
+        return carga;
+    }
+
+    public void setCarga(int carga) {
+        this.carga = carga;
+    }
+
+    public boolean isTampada() {
+        return tampada;
+    }
+
+    public void setTampada(boolean tampada) {
+        this.tampada = tampada;
     }
 
     public void rabiscar()  {
--- src/youtube/cursoemvideo/Main.java
+++ src/youtube/cursoemvideo/Main.java
@@ -3,20 +3,23 @@ package youtube.cursoemvideo;
 public class Main {
     public static void main(String[] args) {
         Caneta c1 = new Caneta();
-        c1.modelo = "BIC";
-        c1.carga = 80;
-        c1.cor = "Preta";
-        //c1.ponta = 0.7f;
+        //c1.setModelo("BIC");
+        c1.setCarga(80);
+        c1.setCor("Preta");
+        //c1.setPonta(0.7f);
         c1.tampar();
         c1.status();
         c1.rabiscar();
 
+        // Antiga fórmula
         Caneta c2 = new Caneta();
-        c2.modelo = "Faber Castel";
+        //c2.modelo = "Faber Castel";
         c2.cor = "Azul";
+       // c2.setPonta(0.5f); //menos esse
         c2.carga = 80;
         c2.destampar();
         c2.status();
+        System.out.println("O melhor modelo de caneta é o " + c2.getModelo()); //e esse
         c2.rabiscar();
     }
 }
